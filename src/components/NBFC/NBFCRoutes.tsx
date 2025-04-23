97fdb3e249b67af0d50ec38c4f0029af6c0d474f
Shashwat Singh <sshashwatssingh@gmail.com>
2025-04-23 17:59:43 +0530

    removed deteled imports

--- src/components/NBFC/NBFCRoutes.tsx
+++ src/components/NBFC/NBFCRoutes.tsx
@@ -3,13 +3,10 @@ import Layout from "../Layout";
 import OnboardingPage from "./OnboardingPage";
 import SegmentSelection from "./SegmentSelection";
 import NBFCform from "./NBFCform";
-import DisbursementFileUpload from "./DisbursementFileUpload";
-import DisbursementFileInfer from "./DisbursementFileInfer";
 import BREConfig from "./BREConfig";
 import LoanProductConfig from "./LoanProductConfig";
 import NotFound from "../NotFound";
 import ContactPage from "../Contact";
-import DisbursementFileRundown from "./DisbursementFileRundown";
 
 const NBFCRoutes = () => {
   return (
@@ -41,35 +38,6 @@ const NBFCRoutes = () => {
           element={
             <Layout>
               <NBFCform />
-            </Layout>
-          }
-        />
-
-        {/* DISBURSEMENT FILE UPLOAD */}
-        <Route
-          path="/nbfc/disbursement-file-upload"
-          element={
-            <Layout>
-              <DisbursementFileUpload />
-            </Layout>
-          }
-        />
-        {/* DISBURSEMENT FILE RUNDOWN */}
-        <Route
-          path="/nbfc/disbursement-file-rundown"
-          element={
-            <Layout>
-              <DisbursementFileRundown />
-            </Layout>
-          }
-        />
-
-        {/* DISBURSEMENT FILE INFER */}
-        <Route
-          path="/nbfc/disbursement-file-infer"
-          element={
-            <Layout>
-              <DisbursementFileInfer />
             </Layout>
           }
         />
