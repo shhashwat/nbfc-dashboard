a85d6ca705c2cc6449e032b36435a48b615137cf
Shashwat Singh <sshashwatssingh@gmail.com>
2025-04-24 16:37:13 +0530

    analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from "@/components/ui/sonner"
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
       <App />
+      <Analytics />
       <Toaster />
     </Router>
   </StrictMode>
